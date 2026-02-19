1464ec12a23b806ddeeb1b1f37cde9eda6933014
Shivani Sharma <shivani@example.com>
2026-02-20 03:04:18 +0530

    Expense API

--- expenses/models.py
+++ expenses/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 
 # Create your models here.
-class Expenses(models.Model):
+class Expense(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE, 
@@ -19,6 +19,13 @@ class Expenses(models.Model):
     note = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-date", "-created_at"]
+        indexes = [
+            models.Index(fields=["user","date"]),
+            models.Index(fields=["user","category"])
+        ]
 
     def __str__(self):
         return f"{self.user} - {self.category} - {self.amount} on {self.date}"
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Expense
+from .serializers import ExpenseSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class ExpenseViewSet(viewsets.ModelViewSet):
+    serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
+
+    # only logged in user expenses 
+    def get_queryset(self):
+        return Expense.objects.filter(user = self.request.user).order_by('-date','-created_at')
+    
+    # ownership enforcement 
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+    
